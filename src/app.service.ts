0cc36e3e924935d6b7b398618856fe608b6da30b
kcfit <keshiacorioll@me.com>
2024-07-02 23:49:46 -0400

    make change to Appservice file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! EC2-2';
+    return 'Hello from Keshia, attempt at a change';
   }
 }
